d8f3065cb26d2361fcb7cfdecb2cd9a008cb55fb
Jerry TOKPON <121157081+jerrytkp@users.noreply.github.com>
2025-02-13 10:36:20 +0100

    Ajout

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { connecter } = require('./bd/connect');
+const { connecter } = require('../../bd/connect');
+
 const app = express();
 const path = require("path");
 
